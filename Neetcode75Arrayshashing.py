f3a5620353cb0b6bce0e3fa2ac66567b58680dcc
Bhashini <bhashiyasam@gmail.com>
2025-05-02 02:23:33 -0400

    longest consecutive

--- Neetcode75Arrayshashing.py
+++ Neetcode75Arrayshashing.py
@@ -189,3 +189,18 @@ def productExceptSelf(self, nums: List[int]) -> List[int]:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+
+# longest consecutive sequence 
+from collections import defaultdict
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mp = defaultdict(int)
+        res = 0
+
+        for num in nums:
+            if not mp[num]:
+                mp[num] = mp[num - 1] + mp[num + 1] + 1
+                mp[num - mp[num - 1]] = mp[num]
+                mp[num + mp[num + 1]] = mp[num]
+                res = max(res, mp[num])
+        return res
